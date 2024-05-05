51f3e49907db63b44d45de74f3cefb9e3e19d71d
pavanpeddini <129878141+pavanpeddini@users.noreply.github.com>
2024-05-05 15:53:40 -0400

    Add files via upload

---
+++ src/main/java/AppWeb/AppWeb/AppWebApplication.java
@@ -0,0 +1,13 @@
+package AppWeb.AppWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/AppWeb/AppWeb/AppWebApplicationTests.java
@@ -0,0 +1,13 @@
+package AppWeb.AppWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
